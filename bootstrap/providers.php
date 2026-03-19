12a93baad5549dfca59e5c709afa307434f1593f
fahrymaodah <mfahry96@gmail.com>
2026-03-19 21:44:43 +0800

    Initial commit: Laravel 13 + Filament 5 + Docker setup

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\CourierPanelProvider::class,
+    App\Providers\Filament\SellerPanelProvider::class,
+];
